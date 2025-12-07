33da6fc43585c406147b867037e4070c2120d296
bvbxbv <aburach@inbox.ru>
2025-12-07 21:15:30 +0700

    fix: fix lyrics output errors on http error
    - fixed lyrics output on http error (with try catch lol)
    - added on_not_found func to remove duplicated errors

--- scripts/main.py
+++ scripts/main.py
@@ -5,6 +5,16 @@ from transliterator import transliterate
 from config import settings
 
 
+def on_not_found():
+    format_output(settings["display"]["empty_filler"])
+    format_output(
+        settings["display"]["empty_filler"]
+        + " "
+        + settings["display"]["lyrics_not_found"]
+    )
+    print_fill(range(settings["fill_lines"] - 2))
+
+
 def run():
     genius = genius_instance()
     query = sys.stdin.read().strip()
@@ -13,21 +23,21 @@ def run():
         print_fill(range(settings["fill_lines"]))
         return
 
-    song = genius.search_song(transliterate(query))
+    song = None
+    try:
+        song = genius.search_song(transliterate(query))
+    except:
+        on_not_found()
+        sys.exit()
+
     if not song or not song.lyrics:
-        format_output(settings["display"]["empty_filler"])
-        format_output(
-            settings["display"]["empty_filler"]
-            + " "
-            + settings["display"]["lyrics_not_found"]
-        )
-        print_fill(range(settings["fill_lines"] - 2))
-        return
+        on_not_found()
+        sys.exit()
 
     lyrics = filter_lyrics(song.lyrics.splitlines())
 
     for line in lyrics:
         format_output(settings["display"]["empty_filler"] + " " + line)
 
-    free_space = settings["lyrics_area_height"] - len(lyrics)
+    free_space = settings["lyrics_area_height"] - len(lyrics) - 1
     print_fill(range(max(0, free_space)), make_genius_url(transliterate(query)))
